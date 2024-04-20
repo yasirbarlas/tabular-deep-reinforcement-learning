47c98c78ebba2beb3a5f99279ce98c8bc3c3a4ba
Yasir Barlas <79994089+yasirbarlas@users.noreply.github.com>
2024-04-21 00:12:38 +0100

    Add files via upload

---
+++ advanced-tasks/PriorExpDQN/agent.py
@@ -0,0 +1,294 @@
+import os
+import pickle
+import numpy as np
+from matplotlib import pyplot as plt
+
+import torch
+import torch.optim as optim
+import torch.nn.functional as F
+
+from gymnasium.wrappers import RecordVideo
+
+from buffers import *
+from models import *
+from utils import *
+
+class PriorExpReplayAgent:
+    """Prioritised Experience Replay DQN Agent interacting with environment
+    
+    Attribute:
+        env (gym.Env): openAI Gym environment
+        memory (ReplayBuffer): replay memory to store transitions
+        batch_size (int): batch size for sampling
+        epsilon (float): parameter for epsilon greedy policy
+        epsilon_decay (float): step size to decrease epsilon
+        max_epsilon (float): max value of epsilon
+        min_epsilon (float): min value of epsilon
+        target_update (int): period for target model's hard update
+        gamma (float): discount factor
+        dqn (Network): model to train and select actions
+        dqn_target (Network): target model to update
+        optimizer (torch.optim): optimizer for training dqn
+        transition (list): transition information including state, action, reward, next_state, done
+        beta (float): determines how much importance sampling is used
+        prior_eps (float): guarantees every transition can be sampled
+    """
+
+    def __init__(self, env, memory_size, batch_size, target_update, seed, gamma = 0.99, max_epsilon = 1.0, min_epsilon = 0.01, epsilon_decay = 0.00001, alpha = 0.5, beta = 0.4, prior_eps = 0.000001, learning_rate = 0.0000625, optimizer = "adam"):
+        """Initialisation
+        
+        Args:
+            env (gym.Env): Gymnasium environment
+            memory_size (int): length of memory
+            batch_size (int): batch size for sampling
+            target_update (int): period for target model's hard update
+            lr (float): learning rate
+            gamma (float): discount factor
+            alpha (float): determines how much prioritization is used
+            beta (float): determines how much importance sampling is used
+            prior_eps (float): guarantees every transition can be sampled
+            v_min (float): min value of support
+            v_max (float): max value of support
+            atom_size (int): the unit number of support
+            n_step (int): step number to calculate n-step td error
+        """
+        obs_dim = env.observation_space.shape
+        action_dim = env.action_space.n
+
+        self.env = env
+        self.batch_size = batch_size
+        self.target_update = target_update
+        self.seed = seed
+        self.gamma = gamma
+        self.epsilon = max_epsilon
+        self.max_epsilon = max_epsilon
+        self.min_epsilon = min_epsilon
+        self.epsilon_decay = epsilon_decay
+        self.alpha = alpha
+        self.beta = beta
+        self.prior_eps = prior_eps
+        
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        print(self.device)
+        
+        # Prioritised Experience Replay
+        self.memory = PrioritizedReplayBuffer(obs_dim, memory_size, batch_size, alpha)
+        
+        # Action space seed
+        self.env.action_space.seed(seed)
+
+        # Neural network models setup
+        self.dqn = SimpleDQN(obs_dim, action_dim).to(self.device)
+        self.dqn_target = SimpleDQN(obs_dim, action_dim).to(self.device)
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
+        self.dqn_target.eval()
+        
+        # Optimizer setup
+        optimizer.lower()
+        if optimizer == "adam":
+            self.optimizer = optim.Adam(self.dqn.parameters(), lr = learning_rate)
+        if optimizer == "rmsprop":
+            self.optimizer = optim.RMSprop(self.dqn.parameters(), lr = learning_rate)
+        if optimizer == "sgd":
+            self.optimizer = optim.SGD(self.dqn.parameters(), lr = learning_rate)
+
+        # Transition to store in memory
+        self.transition = list()
+        
+        # mode: train / test
+        self.is_test = False
+
+    def select_action(self, state):
+        """Select an action from the input state."""
+        selected_action = self.dqn.act(state, self.epsilon)
+        
+        if not self.is_test:
+            self.transition = [state, selected_action]
+        
+        return selected_action
+
+    def step(self, action):
+        """Take an action and return the response of the env."""
+        next_state, reward, terminated, truncated, _ = self.env.step(action)
+        done = terminated or truncated
+
+        if not self.is_test:
+            self.transition += [reward, next_state, done]
+            self.memory.store(*self.transition)
+    
+        return next_state, reward, done
+
+    def update_model(self):
+        """Update the model by gradient descent."""
+        # PER needs beta to calculate weights
+        samples = self.memory.sample_batch(self.beta)
+        weights = torch.tensor(samples["weights"].reshape(-1, 1), dtype = torch.float, device = self.device).to(self.device)
+        indices = samples["indices"]
+
+        # PER: importance sampling before average
+        elementwise_loss = self._compute_dqn_loss(samples)
+        loss = torch.mean(elementwise_loss * weights)
+
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        # PER: update priorities
+        loss_for_prior = elementwise_loss.detach().cpu().numpy()
+        new_priorities = loss_for_prior + self.prior_eps
+        self.memory.update_priorities(indices, new_priorities)
+
+        return loss.item()
+        
+    def train(self, num_frames, plotting_interval = 1000):
+        """Train the agent."""
+        self.is_test = False
+        
+        state, _ = self.env.reset(seed = self.seed)
+        update_cnt = 0
+        epsilons = []
+        losses = []
+        scores = []
+        score = 0
+
+        for frame_idx in range(1, num_frames + 1):
+            # At the halfway point, reduce learning rate by a tenth
+            if frame_idx == int(num_frames // 2):
+                adjust_learning_rate(self.optimizer, 0.1)
+
+            action = self.select_action(state)
+            next_state, reward, done = self.step(action)
+
+            state = next_state
+            score += reward
+
+            # PER: increase beta
+            fraction = min(frame_idx / num_frames, 1.0)
+            self.beta = self.beta + fraction * (1.0 - self.beta)
+
+            # Exponentially decrease epsilon
+            self.epsilon = self.min_epsilon + (self.max_epsilon - self.min_epsilon) * np.exp(- self.epsilon_decay * frame_idx)
+            epsilons.append(self.epsilon)
+
+            # if episode ends
+            if done:
+                state, _ = self.env.reset(seed = self.seed)
+                scores.append(score)
+                score = 0
+
+            # if training is ready
+            if len(self.memory) >= self.batch_size:
+                loss = self.update_model()
+                losses.append(loss)
+                update_cnt += 1
+
+                # if hard update is needed
+                if update_cnt % self.target_update == 0:
+                    self._target_hard_update()
+
+            # Plot and save results, and save models
+            if frame_idx % plotting_interval == 0:
+                self._plot(scores, losses, epsilons)
+                self._save(scores, losses, epsilons)
+
+                print(f"Frame: {frame_idx}, Mean of last 10 rewards: {np.mean(scores[-10:])}")
+
+                # Create checkpoint folder
+                if not os.path.exists("checkpoints"):
+                    os.makedirs("checkpoints")
+
+                # Save the model checkpoint
+                checkpoint_name = f"checkpoint_PER_dqn_latest.pth.tar"
+                checkpoint_path = os.path.join("checkpoints", checkpoint_name)
+                torch.save({"current_model": self.dqn.state_dict(), "target_model": self.dqn_target.state_dict(), "optimizer": self.optimizer.state_dict(), "losses": losses, "rewards": scores, "frame": frame_idx}, checkpoint_path)
+
+        print("Training successfully completed.")
+
+        self.env.close()
+
+    def test(self, video_folder = "per-dqn_agent_video"):
+        """Test the agent."""
+        self.is_test = True
+        
+        # Create checkpoint folder
+        if not os.path.exists(video_folder):
+            os.makedirs(video_folder)
+        
+        # For recording a video of agent
+        naive_env = self.env
+        self.env = RecordVideo(self.env, video_folder = video_folder)
+        
+        state, _ = self.env.reset(seed = self.seed)
+        done = False
+        score = 0
+        
+        while not done:
+            action = self.select_action(state)
+            next_state, reward, done = self.step(action)
+
+            state = next_state
+            score += reward
+        
+        print("Score: ", score)
+        self.env.close()
+        
+        # Reset
+        self.env = naive_env
+
+    def _compute_dqn_loss(self, samples):
+        """Return DQN loss."""
+        state = torch.tensor(samples["obs"], dtype = torch.float, device = self.device).to(self.device)
+        next_state = torch.tensor(samples["next_obs"], dtype = torch.float, device = self.device).to(self.device)
+        action = torch.tensor(samples["acts"].reshape(-1, 1), dtype = torch.long, device = self.device).to(self.device)
+        reward = torch.tensor(samples["rews"].reshape(-1, 1), dtype = torch.float, device = self.device).to(self.device)
+        done = torch.tensor(samples["done"].reshape(-1, 1), dtype = torch.float, device = self.device).to(self.device)
+
+        curr_q_value = self.dqn(state).gather(1, action)
+        next_q_value = self.dqn_target(next_state).max(dim = 1, keepdim = True)[0].detach()
+        mask = 1 - done
+        target = (reward + self.gamma * next_q_value * mask).to(self.device)
+
+        # Calculate DQN loss
+        loss = F.smooth_l1_loss(curr_q_value, target, reduction = "none")
+
+        return loss
+
+    def _target_hard_update(self):
+        """Hard update: target <- local."""
+        self.dqn_target.load_state_dict(self.dqn.state_dict())
+
+    def _save(self, rewards, losses, epsilons):
+        # Save results to a file
+        with open("PER-dqn-results.pkl", "wb") as f:
+            pickle.dump(rewards, f)
+            pickle.dump(losses, f)
+            pickle.dump(epsilons, f)
+
+    def _plot(self, rewards, losses, epsilons):
+        plt.figure(figsize = (40, 6))
+        
+        plt.subplot(131)
+        plt.title("PER-DQN Rewards Per Episode")
+        plt.plot(rewards, label = "Reward")
+        if len(rewards) >= 100:
+            plt.plot(moving_average(rewards), label = "Moving Average", color = "red")
+        plt.xlabel("Episode")
+        plt.ylabel("Reward")
+        plt.legend()
+        
+        plt.subplot(132)
+        plt.title("PER-DQN Loss and Epsilon Per Frame")
+        plt.plot(losses, label = "DQN Loss", color = "tab:blue")
+        plt.xlabel("Frame")
+        plt.ylabel("Loss", color = "tab:blue")
+        plt.tick_params(axis = "y", labelcolor = "tab:blue")
+        plt.legend(loc = "upper left")
+        
+        ax2 = plt.gca().twinx()
+        ax2.plot(epsilons, label = "Epsilon", color = "tab:red")
+        ax2.set_ylabel("Epsilon", color = "tab:red")
+        ax2.tick_params(axis = "y", labelcolor = "tab:red")
+        ax2.legend(loc = "upper right")
+        
+        plt.savefig("PER_dqn_plot.pdf")
+        plt.close()
---
+++ advanced-tasks/PriorExpDQN/buffers.py
@@ -0,0 +1,228 @@
+import operator
+import random
+import numpy as np
+from collections import deque
+
+class SegmentTree:
+    """ Create SegmentTree.
+    Taken from OpenAI baselines github repository:
+    https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
+    Attributes:
+        capacity (int)
+        tree (list)
+        operation (function)
+    """
+    def __init__(self, capacity, operation, init_value):
+        """Initialisation.
+        Args:
+            capacity (int)
+            operation (function)
+            init_value (float)
+
+        """
+        assert (capacity > 0 and capacity & (capacity - 1) == 0), "capacity must be positive and a power of 2."
+        self.capacity = capacity
+        self.tree = [init_value for _ in range(2 * capacity)]
+        self.operation = operation
+
+    def _operate_helper(self, start, end, node, node_start, node_end):
+        """Returns result of operation in segment."""
+        if start == node_start and end == node_end:
+            return self.tree[node]
+        mid = (node_start + node_end) // 2
+        if end <= mid:
+            return self._operate_helper(start, end, 2 * node, node_start, mid)
+        else:
+            if mid + 1 <= start:
+                return self._operate_helper(start, end, 2 * node + 1, mid + 1, node_end)
+            else:
+                return self.operation(self._operate_helper(start, mid, 2 * node, node_start, mid), self._operate_helper(mid + 1, end, 2 * node + 1, mid + 1, node_end))
+
+    def operate(self, start = 0, end = 0):
+        """Returns result of applying `self.operation`."""
+        if end <= 0:
+            end += self.capacity
+        end -= 1
+
+        return self._operate_helper(start, end, 1, 0, self.capacity - 1)
+
+    def __setitem__(self, idx, val):
+        """Set value in tree."""
+        idx += self.capacity
+        self.tree[idx] = val
+
+        idx //= 2
+        while idx >= 1:
+            self.tree[idx] = self.operation(self.tree[2 * idx], self.tree[2 * idx + 1])
+            idx //= 2
+
+    def __getitem__(self, idx):
+        """Get real value in leaf node of tree."""
+        assert 0 <= idx < self.capacity
+
+        return self.tree[self.capacity + idx]
+
+class SumSegmentTree(SegmentTree):
+    """ Create SumSegmentTree.
+    Taken from OpenAI baselines github repository:
+    https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
+    """
+    def __init__(self, capacity):
+        """Initialisation.
+        Args:
+            capacity (int)
+
+        """
+        super(SumSegmentTree, self).__init__(capacity = capacity, operation = operator.add, init_value = 0.0)
+
+    def sum(self, start = 0, end = 0):
+        """Returns arr[start] + ... + arr[end]."""
+        return super(SumSegmentTree, self).operate(start, end)
+
+    def retrieve(self, upperbound):
+        """Find the highest index `i` about upper bound in the tree"""
+
+        assert 0 <= upperbound <= self.sum() + 1e-5, "upperbound: {}".format(upperbound)
+
+        idx = 1
+
+        while idx < self.capacity:  # while non-leaf
+            left = 2 * idx
+            right = left + 1
+            if self.tree[left] > upperbound:
+                idx = 2 * idx
+            else:
+                upperbound -= self.tree[left]
+                idx = right
+        return idx - self.capacity
+
+class MinSegmentTree(SegmentTree):
+    """ Create SegmentTree.
+    Taken from OpenAI baselines github repository:
+    https://github.com/openai/baselines/blob/master/baselines/common/segment_tree.py
+    """
+    def __init__(self, capacity):
+        """Initialisation.
+
+        Args:
+            capacity (int)
+
+        """
+        super(MinSegmentTree, self).__init__(capacity = capacity, operation = min, init_value = float("inf"))
+
+    def min(self, start = 0, end = 0):
+        """Returns min(arr[start], ...,  arr[end])."""
+        return super(MinSegmentTree, self).operate(start, end)
+
+class ReplayBuffer:
+    """A simple numpy replay buffer."""
+
+    def __init__(self, obs_shape, size, batch_size = 32):
+        self.obs_buf = np.zeros([size] + list(obs_shape), dtype = np.float32)
+        self.next_obs_buf = np.zeros([size] + list(obs_shape), dtype = np.float32)
+        self.acts_buf = np.zeros([size], dtype = np.float32)
+        self.rews_buf = np.zeros([size], dtype = np.float32)
+        self.done_buf = np.zeros(size, dtype = np.float32)
+        self.max_size, self.batch_size = size, batch_size
+        self.ptr, self.size, = 0, 0
+
+    def store(self, obs, act, rew, next_obs, done):
+        self.obs_buf[self.ptr] = obs
+        self.next_obs_buf[self.ptr] = next_obs
+        self.acts_buf[self.ptr] = act
+        self.rews_buf[self.ptr] = rew
+        self.done_buf[self.ptr] = done
+        self.ptr = (self.ptr + 1) % self.max_size
+        self.size = min(self.size + 1, self.max_size)
+
+    def sample_batch(self):
+        idxs = np.random.choice(self.size, size = self.batch_size, replace = False)
+
+        return dict(obs = self.obs_buf[idxs], next_obs = self.next_obs_buf[idxs], acts = self.acts_buf[idxs], rews = self.rews_buf[idxs], done = self.done_buf[idxs], indices = idxs)
+
+    def __len__(self):
+        return self.size
+
+class PrioritizedReplayBuffer(ReplayBuffer):
+    """Prioritized Replay Buffer."""
+
+    def __init__(self, obs_shape, size, batch_size = 32, alpha = 0.5):
+        """Initialisation."""
+        assert alpha >= 0
+
+        super(PrioritizedReplayBuffer, self).__init__(obs_shape, size, batch_size)
+        self.max_priority, self.tree_ptr = 1.0, 0
+        self.alpha = alpha
+
+        # capacity must be positive and a power of 2.
+        tree_capacity = 1
+        while tree_capacity < self.max_size:
+            tree_capacity *= 2
+
+        self.sum_tree = SumSegmentTree(tree_capacity)
+        self.min_tree = MinSegmentTree(tree_capacity)
+
+    def store(self, obs, act, rew, next_obs, done):
+        """Store experience and priority."""
+        super().store(obs, act, rew, next_obs, done)
+
+        self.sum_tree[self.tree_ptr] = self.max_priority ** self.alpha
+        self.min_tree[self.tree_ptr] = self.max_priority ** self.alpha
+        self.tree_ptr = (self.tree_ptr + 1) % self.max_size
+
+    def sample_batch(self, beta = 0.4):
+        """Sample a batch of experiences."""
+        assert len(self) >= self.batch_size
+        assert beta > 0
+
+        indices = self._sample_proportional()
+
+        obs = self.obs_buf[indices]
+        next_obs = self.next_obs_buf[indices]
+        acts = self.acts_buf[indices]
+        rews = self.rews_buf[indices]
+        done = self.done_buf[indices]
+        weights = np.array([self._calculate_weight(i, beta) for i in indices])
+
+        return dict(obs = obs, next_obs = next_obs, acts = acts, rews = rews, done = done, weights = weights, indices = indices)
+
+    def update_priorities(self, indices, priorities):
+        """Update priorities of sampled transitions."""
+        assert len(indices) == len(priorities)
+
+        for idx, priority in zip(indices, priorities):
+            assert priority > 0
+            assert 0 <= idx < len(self)
+
+            self.sum_tree[idx] = priority ** self.alpha
+            self.min_tree[idx] = priority ** self.alpha
+
+            self.max_priority = max(self.max_priority, priority)
+
+    def _sample_proportional(self):
+        """Sample indices based on proportions."""
+        indices = []
+        p_total = self.sum_tree.sum(0, len(self) - 1)
+        segment = p_total / self.batch_size
+
+        for i in range(self.batch_size):
+            a = segment * i
+            b = segment * (i + 1)
+            upperbound = random.uniform(a, b)
+            idx = self.sum_tree.retrieve(upperbound)
+            indices.append(idx)
+
+        return indices
+
+    def _calculate_weight(self, idx, beta):
+        """Calculate the weight of the experience at idx."""
+        # Get max weight
+        p_min = self.min_tree.min() / self.sum_tree.sum()
+        max_weight = (p_min * len(self)) ** (-beta)
+
+        # Calculate weights
+        p_sample = self.sum_tree[idx] / self.sum_tree.sum()
+        weight = (p_sample * len(self)) ** (-beta)
+        weight = weight / max_weight
+
+        return weight
